c1141f8a03d00f4f3e57012afdecc5a829484999
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-04-19 11:16:12 -0600

    simple stack approach added

--- Algorithms/StackAlgos/RemovingStarsFromString.java
+++ Algorithms/StackAlgos/RemovingStarsFromString.java
@@ -15,6 +15,26 @@ public class RemovingStarsFromString {
     }
 
     public static String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: s.toCharArray()) {
+            if(c=='*') {
+                if(!stack.isEmpty()) stack.pop();
+            }
+            else stack.push(c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) sb.append(stack.pop());
+        return sb.reverse().toString();
+    }
+
+
+
+
+
+
+    public static String removeStars1(String s) {
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
             if (c == '*') {
