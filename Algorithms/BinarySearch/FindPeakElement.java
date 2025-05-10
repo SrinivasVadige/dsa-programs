32fab66def1711308d971b38e667a15166ce74f3
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-05-10 14:42:11 -0600

    BS: added findPeakElement3() method

--- Algorithms/BinarySearch/FindPeakElement.java
+++ Algorithms/BinarySearch/FindPeakElement.java
@@ -81,6 +81,28 @@ public class FindPeakElement {
 
 
 
+    public static int findPeakElement3(int[] arr) {
+        int n = arr.length;
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+
+        int l=1, r=n-2, mid;
+        while(l<=r) {
+            mid = l + (r-l)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
+            else if(arr[mid]>arr[mid-1]) l=mid+1;
+            else r=mid-1;
+        }
+        return -1;
+    }
+
+
+
+
+
+
+
 
 
 
