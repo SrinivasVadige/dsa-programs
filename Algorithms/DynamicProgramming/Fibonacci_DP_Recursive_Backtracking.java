75da5573e9e0ed9015b64c5f159bbddd0b0a328c
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-05-11 17:58:54 -0600

    DP: fib() recursive backtracking - comments updated

--- Algorithms/DynamicProgramming/Fibonacci_DP_Recursive_Backtracking.java
+++ Algorithms/DynamicProgramming/Fibonacci_DP_Recursive_Backtracking.java
@@ -60,7 +60,7 @@ package Algorithms.DynamicProgramming;
  *
  *
  * <code>"
- * in simple terms, first all the left values are calculated => i.e
+ * in simple terms, first - all the left values are calculated => i.e
  * Once the last left node value (eg: 2) is obtained from method base case or previous call
  * calculate right sibling node value (eg: 1) if we have one, and then add both these siblings in nearest parent node (eg: 3)
  * Here, this nearest parent node is nothing but a left node sibling of another right node,
@@ -69,35 +69,36 @@ package Algorithms.DynamicProgramming;
  * </pre>
  *
  * That's we use recursive approach in graphs and trees
- * <p>
- * <code>
- * <p> SUGGESTIONS FOR FINDING DP PATTERNS:
- * <p>1. Visualize few examples in mind & play around with them
- * <p>2. We see repetitive sub-problem i.e already calculated expression => So, try to implement a sub-problem which can be used to solve the whole problem using loop or recursion
- * <p>3. Find the relationship between sub-problems i.e fib(5) sub-problem should work for fib(4) and all other sub-problems
- * <p>4. Related hard cases to simpler cases i.e fib(4) value should be used for fib(5) and so on
- * <p>5. Tracking & storing the previous sub-problems values in each recursive / loop approach to use them & skip if it is already calculated
- * <p>6. Simplest possible input(s) are the base cases. Eg: fib(1), fib(0)
- * <p>7. Topological sort order - Dependency order of sub-problems -- using sort -- if needed
- * <p>8. Try to construct a directed acyclic graph or binary tree to solve the problem.
- * <p>9. Single directional graph with lot of nodes but not a bi-directional.
- * <p>10. Practice and experience is the key
- * <p>11. First, write the recursive solution and then refactor it to the Top-Down recursive memo or Bottom-Up for loop
- * <p>Egs:
- * <p>* Fibonacci series or nth fibonacci number,
- * <p>* Longest increasing subsequence
- * <p>* Longest common subsequence
- * <p>* Longest palindromic subsequence
- * <p>* Maze problem
- * <p>* 0/1 Knapsack problem
- * <p>* Matrix chain multiplication
- * <p>* subset sum problem
- * <p>* Number of ways to make coins change, so that the coins will be the given Sum
- * <p>* Minimum coins for sum m
- * <p>* Stacking boxes l,w,h -- all available options to stack the boxes like a-box can stack on b-box, c-box can stack on a-box etc i.e it can be a loop too i.e now focus the biggest box as base / root node in each option repeat all created options to find the answer or just keep track of the answer using an extra var like maxLen
- * </code>
+ *
+ * <pre>
+ *
+ *  SUGGESTIONS FOR FINDING DP PATTERNS:
+ * 1. Visualize few examples in mind & play around with them
+ * 2. We see repetitive sub-problem i.e already calculated expression => So, try to implement a sub-problem which can be used to solve the whole problem using loop or recursion
+ * 3. Find the relationship between sub-problems i.e fib(5) sub-problem should work for fib(4) and all other sub-problems
+ * 4. Related hard cases to simpler cases i.e fib(4) value should be used for fib(5) and so on
+ * 5. Tracking & storing the previous sub-problems values in each recursive / loop approach to use them & skip if it is already calculated
+ * 6. Simplest possible input(s) are the base cases. Eg: fib(1), fib(0)
+ * 7. Topological sort order - Dependency order of sub-problems -- using sort -- if needed
+ * 8. Try to construct a directed acyclic graph or binary tree to solve the problem.
+ * 9. Single directional graph with lot of nodes but not a bi-directional.
+ * 10. Practice and experience is the key
+ * 11. First, write the recursive solution and then refactor it to the Top-Down recursive memo or Bottom-Up for loop
+ * Egs:
+ * Fibonacci series or nth fibonacci number,
+ * Longest increasing subsequence
+ * Longest common subsequence
+ * Longest palindromic subsequence
+ * Maze problem
+ * 0/1 Knapsack problem
+ * Matrix chain multiplication
+ * subset sum problem
+ * Number of ways to make coins change, so that the coins will be the given Sum
+ * Minimum coins for sum m
+ * Stacking boxes l,w,h -- all available options to stack the boxes like a-box can stack on b-box, c-box can stack on a-box etc i.e it can be a loop too i.e now focus the biggest box as base / root node in each option repeat all created options to find the answer or just keep track of the answer using an extra var like maxLen
  *
  *
+ * </pre>
  * @TimeComplexity: O(2^n) due to repetitive calls
  * @see <a href="/DataStructures/BinaryTree.java"> /DataStructures/BinaryTree.java </a>
  * @author Srinivas Vadige, srinivas.vadige@gmail.com
@@ -109,7 +110,7 @@ public class Fibonacci_DP_Recursive_Backtracking {
         fib(n);
         // for root node or n = 6 then every recursive node sum and leaves will be printed as =>
         // 1 0 1 1 2 1 0 1 3 1 0 1 1 2 5 1 0 1 1 2 1 0 1 3 8
-        // --> where (1 0 1 1 2 1 0 1 3) 1 0 1 1 2 5 are left sub-tree of parent node i.e 6. and (1 0 1 1 2 1 0 1 3) upto n=4 node
+        // --> where (1 0 1 1 2 1 0 1 3) 1 0 1 1 2 5 are left sub-tree of parent node i.e 6. and (1 0 1 1 2 1 0 1 3) up to n=4 node
 
         // and for n=8 then every recursive sum (only temp not leaves) will be printed as =>
         // 1 2 1 3 1 2 5 1 2 1 3 8 1 2 1 3 1 2 5 13 1 2 1 3 1 2 5 1 2 1 3 8 21
@@ -121,7 +122,7 @@ public class Fibonacci_DP_Recursive_Backtracking {
             return n;
         }
         int temp = fib(n - 1) + fib(n - 2); // or int temp = fib(n - 2) + fib(n - 1);
-        System.out.println( "node: " + n + ", " + "sum of " + (n-1) + " and " + (n-2) + " nodes: " + temp); // prints repeatative calls as well
+        System.out.println( "node: " + n + ", " + "sum of " + (n-1) + " and " + (n-2) + " nodes: " + temp); // prints repetitive calls as well
         return temp;
     }
 }
