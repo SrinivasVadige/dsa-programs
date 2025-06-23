5eaa764ee01195d5ed92ed0002939cf99a2e3037
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-06-22 21:53:01 -0600

    int[]: InsertDeleteGetRandom updated comments

--- Algorithms/IntegerArray/InsertDeleteGetRandom.java
+++ Algorithms/IntegerArray/InsertDeleteGetRandom.java
@@ -26,35 +26,39 @@ public class InsertDeleteGetRandom {
     }
 
 
-
-
+    /**
+     * NOTE:
+     * 1. TimeComplexity of arrayList.remove(0) is O(n) and linkedList.remove(0) is O(1)
+     * 2. Similarly, TimeComplexity of arrayList.remove(n-1) is O(1) and linkedList.remove(n-1) is O(n)
+     */
     static class RandomizedSet{
-        private HashMap<Integer, Integer> map;
-        private ArrayList<Integer> list;
-        private Random random;
+        private final HashMap<Integer, Integer> map; // <val, valI>
+        private final ArrayList<Integer> list;
 
         public RandomizedSet() {
             map = new HashMap<>();
             list = new ArrayList<>();
-            random = new Random();
         }
 
         public boolean insert(int val) {
             if (map.containsKey(val)) return false;
-            map.put(val, list.size());
-            list.add(val);
+            // eg: <val, valI> = <3, 5>
+            map.put(val, list.size()); // <val, valI> -- because we insert "val" at the end of the list
+            list.add(val); // the new size of the list is 6 i.e., --> 5 is the last element index i.e the position of 3 in the list is 5th index
             return true;
         }
 
         public boolean remove(int val) {
             if (!map.containsKey(val)) return false;
 
-            Integer index = map.get(val);
-            Integer lastElement = list.get(list.size() - 1);
+            // replace the val position with the lastVal and then remove the lastVal
+            // --- so we decreased the list size by 1 by removing the lastVal
+            int valI = map.get(val);
+            int lastVal = list.get(list.size() - 1);
 
-            //swap i.e Move last element to 'index' of val
-            list.set(index, lastElement);
-            map.put(lastElement, index);
+            //swap i.e Move lastVal to 'valI' index i.e the position of val --- this means we deleted the val from the list
+            list.set(valI, lastVal);
+            map.put(lastVal, valI);
 
             // Remove last
             list.remove(list.size() - 1);
@@ -64,7 +68,7 @@ public class InsertDeleteGetRandom {
         }
 
         public int getRandom() {
-            return list.get(random.nextInt(list.size()));
+            return list.get(new Random().nextInt(list.size())); // create a class Random variable and just use random.nextInt(size);
         }
     }
 
