04d8a77181811581597df92d6553cf8f63a31331
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-07-23 19:40:47 -0600

    added more methods

--- Algorithms/DynamicProgramming/LongestCommonSubsequence.java
+++ Algorithms/DynamicProgramming/LongestCommonSubsequence.java
@@ -1,9 +1,6 @@
 package Algorithms.DynamicProgramming;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -42,17 +39,32 @@ import java.util.stream.Collectors;
  *
  * Look diagonally only when str1[i] == str2[j] i.e characters matched && ++ the diagonal value
  *
+ *
+ * This Longest Common Subsequence problem looks like the combination of:
+ * 1) Structurally like 0-1 Knapsack - {@link Algorithms.DynamicProgramming.Knapsack_01_DP_SubsetSumProblem}
+ * 2) Semantically tied to Levenshtein Distance - {@link Algorithms.DynamicProgramming.EditDistance}
+ *
+ * It's like 0-1 Knapsack problem -- cause let's say we have multiple same chars in different positions in text1 & text2
+ * so, we can consider the first occurrence of same char or not -- or second occurrence or not -- or third occurrence or not........
+ *
+ *
  * </pre>
  * @author Srinivas Vadige, srinivas.vadige@gmail.com
  * @since 07 Nov 2024
+ * @link 1143. Longest Common Subsequence <a href="https://leetcode.com/problems/longest-common-subsequence/">LeetCode link</a>
+ * @topics String, Dynamic Programming
+ * @companies Salesforce, Amazon, Meta, Microsoft, Google, Bloomberg, Oracle, DoorDash, Adobe, ByteDance, Walmart Labs, TikTok, Yahoo, BP, Optum
  */
 public class LongestCommonSubsequence {
 
     public static void main(String[] args) {
         String text1 = "abcdgh";
         String text2 = "aedfhr";
-        System.out.println("longestCommonSubsequenceBottomUpTabulationDp: " + longestCommonSubsequenceBottomUpTabulationDp(text1, text2));
-        System.out.println("longestCommonSubsequenceTopDownDp: " + longestCommonSubsequenceTopDownDp(text1, text2));
+        System.out.println("longestCommonSubsequence BruteForce By AllPairs: " + longestCommonSubsequenceBruteForceByAllPairs(text1, text2));
+        System.out.println("longestCommonSubsequence TopDownDp but TimeNotOptimized: " + longestCommonSubsequenceTopDownDpButTimeNotOptimized(text1, text2));
+        System.out.println("longestCommonSubsequence TopDownDp: " + longestCommonSubsequenceTopDownDp(text1, text2));
+        System.out.println("longestCommonSubsequence BottomUpTabulationDp: " + longestCommonSubsequenceBottomUpTabulationDp(text1, text2));
+        System.out.println("longestCommonSubsequence BottomUpTabulationDp and SpaceOptimized: " + longestCommonSubsequenceBottomUpTabulationDpSpaceOptimized(text1, text2));
     }
 
     /**
@@ -60,65 +72,113 @@ public class LongestCommonSubsequence {
      * @SpaceComplexity O(m*n)
      *
      * Just like {@link Algorithms.DynamicProgramming.HouseRobber#robBottomUpNoMemory(int[])} maintain the max CommonSubsequence in dp[i][j] & ↖ ++ if same char
-     *
-     * Another example:
-     *
-     * "ylqpejqbalahwr"
-     * "yrkzavgdmdgtqpg"
-     *  012345678901234
-     *
-     *                ""    y    l   q    p    e    j    q    b    a    l    a    h    w    r
-     *            _______|____|____|____|____|____|____|____|____|____|____|____|____|____|____|
-     *            ""|  0 | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  |
-     *            y |  0 | ↖1 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  |
-     *            r |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | ↖2 |
-     *            k |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  |
-     *            z |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  |
-     *            a |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | ↖2 | 1  |↖2 | 1  | 1  | 2  |
-     *            v |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
-     *            g |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
-     *            d |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
-     *            m |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
-     *            d |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
-     *            g |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
-     *            t |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
-     *            q |  0 | 1  | 1  | ↖2 | 2  | 2  | 2  |↖2 | 2  | 2  | 2  | 2  | 2  | 2  | 2  |
-     *            p |  0 | 1  | 1  | 1  |↖3  | 3  | 3  | 3 | 3  | 3  | 3  | 3  | 3  | 3  | 3  |
-     *            g |  0 | 1  | 1  | 1  | 3  | 3  | 3  | 3 | 3  | 3  | 3  | 3  | 3  | 3  | 3  |
-     *
+
+      Another example:
+
+      "ylqpejqbalahwr"
+      "yrkzavgdmdgtqpg"
+       012345678901234
+
+                     ""    y    l   q    p    e    j    q    b    a    l    a    h    w    r
+                 _______|____|____|____|____|____|____|____|____|____|____|____|____|____|____|
+                 ""|  0 | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  | 0  |
+                 y |  0 | ↖1 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  |
+                 r |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | ↖2 |
+                 k |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  |
+                 z |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  |
+                 a |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | ↖2 | 1  |↖2  | 1  | 1  | 2  |
+                 v |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
+                 g |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
+                 d |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
+                 m |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
+                 d |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
+                 g |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
+                 t |  0 | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 1  | 2  | 2  | 2  | 2  | 2  | 2  |
+                 q |  0 | 1  | 1  | ↖2 | 2  | 2  | 2  | ↖2 | 2  | 2  | 2  | 2  | 2  | 2  | 2  |
+                 p |  0 | 1  | 1  | 2  | ↖3 | 3  | 3  | 3  | 3  | 3  | 3  | 3  | 3  | 3  | 3  |    <--- understanding this row then you will understand this "Max(←,↑) RECURRENCE RELATION"
+                 g |  0 | 1  | 1  | 1  | 3  | 3  | 3  | 3  | 3  | 3  | 3  | 3  | 3  | 3  | 3  |
+
      */
     public static int longestCommonSubsequenceBottomUpTabulationDp(String text1, String text2) {
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1]; // +1 for ""
-
-        for (int i = 0; i <= text1.length(); i++) {
-            for (int j = 0; j <= text2.length(); j++) {
-                if (i == 0 || j == 0)
-                    dp[i][j] = 0;
-                else if (text1.charAt(i - 1) == text2.charAt(j - 1)) // --- characters matched
-                    dp[i][j] = 1 + dp[i - 1][j - 1]; // ↖ + 1
-                else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // Max(←,↑) RECURRENCE RELATION
-            }
-        }
-        return dp[text1.length()][text2.length()];
-    }
-
-    public static int longestCommonSubsequenceBottomUpTabulationDp2(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
         int[][] dp = new int[m+1][n+1]; // by default all values are 0 so we don't need to set 0 if (i == 0 || j == 0) again
 
-        for(int i=1; i<=m; i++)
+        for(int i=1; i<=m; i++) {
             for(int j=1; j<=n; j++) {
-                if(text1.charAt(i-1) == text2.charAt(j-1))
+                if (i==0 || j==0) dp[i][j] = 0; // optional as int[][] by default all values are 0
+                else if(text1.charAt(i-1) == text2.charAt(j-1)) // --- characters matched
                     dp[i][j] = dp[i-1][j-1]+1; // ↖ + 1
                 else
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); // Max(←,↑)
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); // Max(←,↑) RECURRENCE RELATION
             }
+        }
 
         return dp[m][n];
 
     }
+
+
+
+
+    /**
+     * @TimeComplexity O(M*N)
+     * @SpaceComplexity O(M)
+     *
+     * same like {@link Algorithms.DynamicProgramming.EditDistance#minDistanceUsingLevenshteinDistanceBottomUpTabulationDpOptimized}
+     *
+                so instead of 2d array
+
+                             j-1  j
+                            +---+---+
+                        i-1 | a | b |   ← top row
+                            +---+---+
+                         i  | c | ❓|   ← curr row
+                            +---+---+
+
+
+                use 1d array
+                               j-1      j
+                            +-------+-------+
+                        i-1 | prevA | prevB |   ← prev[] = previous row = topRow
+                            +-------+-------+
+                         i  | currC |   ❓  |   ← curr[] = current row = currRow
+                            +-------+-------+
+     */
+    public static int longestCommonSubsequenceBottomUpTabulationDpSpaceOptimized(String text1, String text2) {
+
+        if (text2.length() < text1.length()) { // make text2 bigger
+            String temp = text1;
+            text1 = text2;
+            text2 = temp;
+        }
+
+        int m = text1.length();
+        int n = text2.length();
+
+        int[] previous = new int[m + 1];
+        int[] current = new int[m + 1];
+
+        for (int c = n-1 ; c >= 0; c--) { // col - text2
+            for (int r = m-1; r >= 0; r--) { // row - text1
+                if (text1.charAt(r) == text2.charAt(c)) {
+                    current[r] = 1 + previous[r + 1];
+                } else {
+                    current[r] = Math.max(previous[r], current[r + 1]);
+                }
+            }
+            int[] temp = previous;
+            previous = current;
+            current = temp;
+        }
+
+        return previous[0];
+    }
+
+
+
+
+
 
     /**
      * <pre>
@@ -144,10 +204,9 @@ public class LongestCommonSubsequence {
      * @SpaceComplexity O(m*n)
      */
     public static int longestCommonSubsequenceTopDownDp(String text1, String text2) {
-        int dp[][] = new int[text1.length()+1][text2.length()+1];
-        for(int i =0;i<text1.length()+1;i++){
-            for(int j =0;j<text2.length()+1;j++)
-                dp[i][j] = -1;
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        for(int i =0; i<text1.length()+1; i++){
+            Arrays.fill(dp[i], -1);
         }
         return rec(text1, text2, 0, 0, dp); // dp[0][0] i.e inside the recursion it calculates form dp[m][n] to dp[0][0] unlike above tabulation
     }
@@ -155,14 +214,13 @@ public class LongestCommonSubsequence {
     private static int rec(String text1, String text2, int i, int j, int[][] dp){
         if(i == text1.length() || j == text2.length()) // when m,n or in above -1 loop skip the m,n as by default dp[][] values are 0
             return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
 
-        int ans = 0; // optional, we can directly set dp[i][j] in below conditions
+        int ans; // optional, we can directly set dp[i][j] in below conditions
         if(text1.charAt(i) == text2.charAt(j))
-            ans =  1 + rec(text1, text2, i+1,j+1, dp);
+            ans =  rec(text1, text2, i+1,j+1, dp) + 1; // ↘ + 1
         else
-            ans = Math.max(rec(text1, text2, i,j+1, dp), rec(text1, text2, i+1,j, dp));
+            ans = Math.max(rec(text1, text2, i+1,j, dp), rec(text1, text2, i,j+1, dp)); // Max(↓,→)
 
         return dp[i][j] = ans;
     }
@@ -174,71 +232,124 @@ public class LongestCommonSubsequence {
 
 
 
+
+
+
     /**
-     * <pre>
-     * MY INITIAL THOUGHTS:
-     * -------------------
-     * abcde
-     * ace
-     * ANS: 3 (ace len)
-     *
-     * abde
-     * ace
-     * ANS: 2 (ae len)
-     *
-     * patterns
-     * a
-     * ac ❌ -- not matched so skip ac
-     * ae
-     *
-     * c ❌ -- stop c sequence
-     *
-     * e
-     *
-     * same like non-contiguous sub arr sum / PartitionEqualSubsetSum --> but we donno where to start in other string, cause we have duplicate start chars
-     *
-     * -- to check subText in t2, convert them to two arrays and remove the char in subText if you found it
-     *
-     * memo hashMap<String, Boolean> each subText and use it in bigger subText
-     *
-     * 2^n graph
-     *
-     * abde
-     * ace
-     * 012
-     * ANS: 2 (ae len)
-     *
-     * -1                                           ""
-     * 0                                    a                 ""
-     * 1                                ac      a         c        ""
-     * 2                                     ae   a     ce c      e  ""
-     *
-     * i.e loop up to t1.length()
-     * Note that we can have duplicate chars too
-     * </pre>
+     * @TimeComplexity O(M*N^2) ---> TimeLimitExceeded / MemoryLimitExceeded
+     * @SpaceComplexity O(N^2)
+     * text1 = "ubmrapg" -> ""
+     * text2 = "ezupkr"
      */
-    @SuppressWarnings("unused")
-    public int longestCommonSubsequenceMyApproach(String text1, String text2) {
-        Map<String, Boolean> map = new HashMap<>();
-        map.put("", true);
-
-        Map<Character, Integer> t1chars = text1.chars().mapToObj(i->(char)i).collect(Collectors.groupingBy(i->i, Collectors.summingInt(e->1)));
-
-        for (int i=text2.length()-1; i<=0; i--) {
-            if(t1chars.containsKey(text2.charAt(i))){
-                for (int j=i; j<text2.length()-1; j++) {
-                    String temp = ""; // add the longest matching subStr and maintain all the possible subStrs in map
-                    //checkSubStr(map, t1chars, i, j);
-                    // we can have duplicate chars i.e multiple i char at multiple positions.. so, where do you start?
-                    // it'll work
-
-
-                }
-            }
-
+    public static int longestCommonSubsequenceBruteForceByAllPairs(String text1, String text2) {
+        if (text1.length() < text2.length()) { // Make text1 the longer one for fewer subsequences
+            String temp = text1;
+            text1 = text2;
+            text2 = temp;
         }
-        return 0;
+
+        List<String> allSubsequences = new ArrayList<>();
+        generateSubsequences(text1, 0, new StringBuilder(), allSubsequences);
+
+        int maxLen = 0;
+        for (String sub : allSubsequences) {
+            if (isSubsequence(sub, text2)) {
+                maxLen = Math.max(maxLen, sub.length());
+            }
+        }
+
+        return maxLen;
     }
+
+    // Generate all subsequences of text
+    private static void generateSubsequences(String text, int index, StringBuilder current, List<String> result) {
+        if (index == text.length()) {
+            result.add(current.toString());
+            return;
+        }
+
+        // Exclude current character
+        generateSubsequences(text, index + 1, current, result);
+
+        // Include current character
+        current.append(text.charAt(index));
+        generateSubsequences(text, index + 1, current, result);
+        current.deleteCharAt(current.length() - 1); // reset backtrack
+
+    }
+
+    // Check if sub is a subsequence of full
+    private static boolean isSubsequence(String sub, String full) {
+        int i = 0, j = 0;
+        while (i < sub.length() && j < full.length()) {
+            if (sub.charAt(i) == full.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i == sub.length();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static int[][] memo;
+    private static String text1;
+    private static String text2;
+
+    /**
+     * @TimeComplexity O(M*N^2)
+     * @SpaceComplexity O(M*N)
+     */
+    public static int longestCommonSubsequenceTopDownDpButTimeNotOptimized(String text1, String text2) {
+        LongestCommonSubsequence.memo = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                LongestCommonSubsequence.memo[i][j] = -1;
+            }
+        }
+        LongestCommonSubsequence.text1 = text1;
+        LongestCommonSubsequence.text2 = text2;
+        return memoSolve(0, 0);
+    }
+
+    private static int memoSolve(int p1, int p2) {
+        if (memo[p1][p2] != -1) {
+            return memo[p1][p2];
+        }
+
+        int option1 = memoSolve(p1 + 1, p2); // Option 1: we don't include text1[p1] in the solution.
+
+        int firstOccurence = text2.indexOf(text1.charAt(p1), p2); // Option 2: We include text1[p1] in the solution, as long as a match for it in text2 at or after p2 exists.
+        int option2 = 0;
+        if (firstOccurence != -1) {
+            option2 = 1 + memoSolve(p1 + 1, firstOccurence + 1);
+        }
+
+        memo[p1][p2] = Math.max(option1, option2);
+        return memo[p1][p2];
+    }
+
+
+
+
+
+
+
+
+
+
 
 
     /**
@@ -246,7 +357,7 @@ public class LongestCommonSubsequence {
      * But we're not sure whether to choose 2nd char are lcs or 3rd char in lcs so that we'll get bigger lcs
      */
     @SuppressWarnings("unused")
-    public int longestCommonSubsequenceMyApproach2(String text1, String text2) {
+    public int longestCommonSubsequenceMyApproachNotWorking(String text1, String text2) {
         String min = "", max = "";
 
         if (text1.length()<text2.length()) {
