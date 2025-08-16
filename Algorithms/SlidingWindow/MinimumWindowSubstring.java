eef831933c16e2c7c3100d653845d19858a80761
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-08-16 09:20:15 -0600

    Updated methods

--- Algorithms/SlidingWindow/MinimumWindowSubstring.java
+++ Algorithms/SlidingWindow/MinimumWindowSubstring.java
@@ -20,12 +20,123 @@ public class MinimumWindowSubstring {
     public static void main(String[] args) {
         String s = "ADOBECODEBANC";
         String t = "ABC";
+        System.out.println("minWindow Using Resetting SlidingWindow (isMatch validation) => " + minWindowUsingResettingSlidingWindow(s, t));
+        System.out.println("minWindow using ASCII Array SlidingWindow => " + minWindowUsingAsciiArrSlidingWindow(s, t));
         System.out.println("minWindow Using SlidingWindow => " + minWindowUsingSlidingWindow(s, t));
         System.out.println("minWindow Using SlidingWindow Optimized => " + minWindowUsingSlidingWindowOptimized(s, t));
         System.out.println("minWindow BruteForce => " + minWindowBruteForce(s, t));
-        System.out.println("minWindow Using Resetting SlidingWindow => " + minWindowUsingResettingSlidingWindow(s, t));
-        System.out.println("minWindow Using SlidingWindow2 => " + minWindowUsingSlidingWindow2(s, t));
     }
+
+
+
+        /**
+     * WORKING but slower than above
+     * @TimeComplexity O(m*o), where m=s.length, n=t.length, o=tMap.size
+     * @SpaceComplexity O(m+n)
+     *
+
+        s="aaaaaaaaaaaabbbbbcddefg"
+        t="abcdd"
+
+        tMap - {a=1, b=1, c=1, d=2}
+
+        a a a a a a a a a a a a b b b b b c d d e f g
+                                l               r
+     */
+    public static String minWindowUsingResettingSlidingWindow(String s, String t) { // UsingSlidingWindowBruteForceIsMatchValidation
+        int l = 0;
+        int m = s.length();
+        String res = "";
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+        for(char c : t.toCharArray()) {
+            tMap.merge(c, 1, Integer::sum);
+        }
+
+        for(int r=0; r<m; r++) {
+            char c = s.charAt(r);
+            sMap.merge(c, 1, Integer::sum);
+            while(l<=r && isMatch(sMap, tMap)) {
+                c = s.charAt(l);
+                sMap.merge(c, -1, Integer::sum);
+
+                if(res.isEmpty() || res.length() > r-l+1) {
+                    res = s.substring(l, r+1);
+                }
+
+                l++;
+            }
+        }
+
+        return res;
+
+    }
+
+    private static boolean isMatch(Map<Character, Integer> sMap, Map<Character, Integer> tMap) {
+        // return tMap.keySet().stream().allMatch(c-> tMap.get(c) <= sMap.getOrDefault(c,0));
+        for(char c : tMap.keySet()) {
+            if(sMap.getOrDefault(c, 0) < tMap.get(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * @TimeComplexity O(m+n)
+     * @SpaceComplexity O(m+n)
+     *
+     * Same like {@link #minWindowUsingResettingSlidingWindow} but use "NEED" and "have" instead of isMatch()
+     */
+    public static String minWindowUsingAsciiArrSlidingWindow(String s, String t) { // UsingAsciiArrSlidingWindow
+        int[] res = {0, -1};
+
+        int[] tCounter = new int['z'-'A'+1]; // 'A'=65, 'Z'=90, 'a'=97, 'z'=122 ---> 122-65+1 = 122-64 = 58
+        int[] sCounter = new int['z'-'A'+1];
+
+        for(char c: t.toCharArray()) {
+            tCounter[c-'A']++;
+        }
+
+        final int NEED = (int) Arrays.stream(tCounter).filter(x->x>0).count();
+        int have = 0; // do have++ only when c frequency in sMap.get(c) == tMap.get(c), and have-- when c in sMap.get(c) < tMap.get(c)
+        int l = 0;
+
+        for(int r=0; r<s.length(); r++) {
+            char c = s.charAt(r);
+
+            sCounter[c-'A']++;
+
+            if(tCounter[c-'A'] == sCounter[c-'A']) {
+                have++;
+            }
+
+            while(have == NEED) {
+                if(res[1]==-1 || res[1]-res[0]>r-l) {
+                    res[0] = l;
+                    res[1] = r;
+                }
+                c = s.charAt(l);
+                sCounter[c-'A']--;
+
+                if(tCounter[c-'A'] > sCounter[c-'A']) {
+                    have--;
+                }
+                l++;
+            }
+        }
+
+        return s.substring(res[0], res[1]+1);
+    }
+
+
+
+
+
+
+
 
 
     /**
@@ -55,7 +166,7 @@ public class MinimumWindowSubstring {
             char c = s.charAt(r);
             windowCounts.merge(c, 1, Integer::sum);
 
-            if (tMap.containsKey(c) && windowCounts.get(c).equals(tMap.get(c))) {
+            if (tMap.containsKey(c) && windowCounts.get(c).equals(tMap.get(c))) { // or tMap.getOrDefault(c, 0).equals(sMap.get(c))
                 have++;
             }
 
@@ -68,7 +179,7 @@ public class MinimumWindowSubstring {
                 }
 
                 windowCounts.merge(c, -1, Integer::sum);
-                if (tMap.containsKey(c) && windowCounts.get(c) < tMap.get(c) ) {
+                if (tMap.containsKey(c) && windowCounts.get(c) < tMap.get(c) ) { // or tMap.getOrDefault(c, 0) > sMap.get(c)
                     have--;
                 }
                 l++;
@@ -187,198 +298,4 @@ public class MinimumWindowSubstring {
         return true;
     }
 
-
-
-
-
-
-
-
-    /**
-     * WORKING but slower than above
-     * @TimeComplexity O(m*o), where m=s.length, n=t.length, o=tMap.size
-     * @SpaceComplexity O(m+n)
-     *
-
-        s="aaaaaaaaaaaabbbbbcddefg"
-        t="abcdd"
-
-        tMap - {a=1, b=1, c=1, d=2}
-
-        a a a a a a a a a a a a b b b b b c d d e f g
-                                l               r
-     */
-    public static String minWindowUsingResettingSlidingWindow(String s, String t) {
-        Map<Character, Integer> tMap = new HashMap<>();
-        for(char c: t.toCharArray()) {
-            tMap.merge(c, 1, Integer::sum);
-        }
-
-        int minL = 0, minR = -1;
-        int m = s.length();
-        int l=0, r=0;
-
-        Map<Character, Integer> sMap = new HashMap<>();
-        while(r<m) {
-            // Move r pointer to right
-            while(r<m && !isEqual(tMap, sMap)) {
-                sMap.merge(s.charAt(r++), 1, Integer::sum);
-            }
-            r--;
-
-            // Move l pointer to right
-            boolean isBreak = false;
-            while(l<=r) {
-                if(isEqual(tMap, sMap)) {
-                    if((minR == -1 || (minR-minL) > (r-l))) {
-                        minR = r;
-                        minL = l;
-                    }
-                    sMap.merge(s.charAt(l), -1, Integer::sum);
-                } else {
-                    isBreak = true;
-                    break;
-                }
-                l++;
-            }
-
-            // when l==r in above while loop then Reset sMap --> start fresh
-            if(isBreak) {
-                r++;
-            } else {
-                l=++r;
-                sMap = new HashMap<>();
-            }
-        }
-        return s.substring(minL, minR+1);
-    }
-    private static boolean isEqual(Map<Character, Integer> tMap, Map<Character, Integer> sMap) {
-        return tMap.keySet().stream().allMatch(k-> tMap.get(k) <= sMap.getOrDefault(k,0));
-        /*
-            // or
-            for(char c: tMap.keySet()) {
-                if(tMap.get(c) > sMap.getOrDefault(c, 0)) {
-                    return false;
-                }
-            }
-            return true;
-         */
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-      "TO_FIND" counter is length of "t" and it is constant
-      "found" counter is used to store the count of t characters in the current subString or window.
-      So, max value of found counter is TO_FOUND t length i.e always foundCounter <= TO_FIND
-      if found < TO_FIND ---> then move right pointer to find the remaining t chars. And check if we found remaining t char then increase found counter and "decrease" charsMap or ascii array int[128] specific char by 1
-      main while loop with right++
-      charsMap.get('right') >= 0 then found++
-      if found == TO_FIND ---> then move left pointer (as we got all t chars & we want min subStr) & check if the current left index char is in t string chars, if it is then decrease found counter and "increase" charsMap or ascii array int[128] specific char by 1 as we need to find that in future
-      sub while loop with left++
-      charsMap.get('left') > 0 then found--
-      i.e iterate main loop until found == TO_FIND then loop sub while loop and then repeat the process untill
-      Remember to always decrease the right pointer charsMap, ascii array char count by 1 in the parent while loop
-      NOTE
-      1. we can use charsMap with char counter or ascii array new int[128] with ascii char counter to store the t chars and visited s chars. Because s and t consist of uppercase and lowercase English letters only
-      2. And specific s chars are always negative i.e <= -1. And t specific char counter max value is it's repition in t string and min value of negative as per presense in s.
-      3. if the t char in charsMap is > 0 then it means we need find that char in s substring that many times.
-      4. while loop instead of for ---> while(rightIndex < s.length) even though we didn't loop till leftIndex in the rightIndex < s.length last loop we have the TO_FIND == found while loop to complete the left pointer
-    */
-      /**
-      * @TimeComplexity - O(m+n)
-    */
-    public static String minWindowUsingSlidingWindow2(String s, String t) {
-        if(t.length() > s.length()) return "";
-        String subStr = "";
-        final int TO_FIND_TARGET = t.length();
-        int found = 0;
-        int left=0, right=0;
-
-        Map<Character, Integer> charsMap = t.chars().mapToObj(e->(char)e).collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e->1)) );
-
-        while(right<s.length()){ // --- right pointer loop (main expression)
-
-            charsMap.merge(s.charAt(right), -1, Integer::sum);
-
-            if(charsMap.get(s.charAt(right)) >= 0 ){
-                found++;
-            }
-
-
-            while(found==TO_FIND_TARGET){
-                int currWindowL = right - left + 1;
-                if(subStr.length() > currWindowL || subStr.isEmpty()){
-                    subStr = s.substring(left, right+1);
-                }
-
-                charsMap.merge(s.charAt(left), 1, Integer::sum);
-
-                if(charsMap.get(s.charAt(left)) > 0){
-                    found--;
-                }
-                left++;
-            }
-
-            right++;
-        }
-        return subStr;
-    }
-
-
-
-
-
-
-
-    public String minWindowUsingAsciiArrSlidingWindow(String s, String t) {
-        // create count array for t and char array for s
-        char[] sArr = s.toCharArray();
-        int[] tCount = new int[128];
-
-        // count the occurrences of each char in t
-        for (char ch : t.toCharArray()) {
-            tCount[ch]++;
-        }
-
-        int required = t.length();
-        int left = 0, right = 0, min = Integer.MAX_VALUE, start = 0;
-
-        // until end of the char array of s
-        while (right < sArr.length) {
-            // check if char at right index is required
-            if (tCount[sArr[right]] > 0) {
-                required--;
-            }
-            tCount[sArr[right]]--;
-            right++;
-
-            // if all chars are found in current window
-            while (required == 0) {
-                // update the prev min if this min is minimun
-                if (right - left < min) {
-                    start = left;
-                    min = right - left;
-                }
-
-                // slide the left pointer towards right
-                if (tCount[sArr[left]] == 0) {
-                    required++;
-
-                }
-                tCount[sArr[left]]++;
-                left++;
-            }
-        }
-        return min == Integer.MAX_VALUE ? "" : new String(sArr, start, min);
-    }
 }
