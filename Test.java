bdf7eefa00b918eaee90110c6e401d78f7018085
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-09-01 19:06:17 -0600

    added all java imports for my practice

--- Test.java
+++ Test.java
@@ -1,13 +1,18 @@
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import java.util.function.*;
+
 @SuppressWarnings("unused")
 public class Test {
 
 /**
  * Practice your Java doubts here ---------->
- */
+*/
 	public static void main(String[] args) {
 
 
 	}
-
 
 }
