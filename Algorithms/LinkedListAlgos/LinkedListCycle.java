7de737a929e5dc87ecc6e7190f741c5998014653
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-10-24 19:47:08 -0600

    Updated documentation

--- Algorithms/LinkedListAlgos/LinkedListCycle.java
+++ Algorithms/LinkedListAlgos/LinkedListCycle.java
@@ -10,9 +10,11 @@ import java.util.Set;
 /**
  * @author Srinivas Vadige, srinivas.vadige@gmail.com
  * @since 26 Nov 2024
+ * @link 141. Linked List Cycle <a href="https://leetcode.com/problems/linked-list-cycle">LeetCode link</a>
+ * @topics Linked List, Two Pointers, Hash Table
  */
 public class LinkedListCycle {
-    private static class ListNode{int val;ListNode next; ListNode(int val){this.val = val;}}
+    public static class ListNode{int val;ListNode next; ListNode(int val){this.val = val;}}
 
     public static void main(String[] args) {
         ListNode head = new ListNode(3);
@@ -20,19 +22,18 @@ public class LinkedListCycle {
         head.next.next = new ListNode(0);
         head.next.next.next = new ListNode(-4);
         head.next.next.next.next = head.next;
-        System.out.println("\nhasCycleUsing2PointersWhileLoop: " + hasCycleUsing2PointersWhileLoop(head));
+        System.out.println("hasCycleUsing2PointersWhileLoop: " + hasCycleUsing2PointersWhileLoop(head));
         System.out.println("hasCycleUsing2PointersForLoop: " + hasCycleUsing2PointersForLoop(head));
         System.out.println("hasCycleUsingIntegerMaxValue: " + hasCycleUsingIntegerMaxValue(head));
 
     }
 
     /**
-     * At some point fast pointer will definitely meet the slow pointer if it's a cyclic
+     * At some point, the fast pointer will definitely meet the slow pointer if it's a cyclic
      */
     public static boolean hasCycleUsing2PointersForLoop(ListNode head) {
-        if(head == null || head.next == null) return false;
-        for(ListNode s=head, f=head.next; s!=null && f!=null; s=s.next, f=f.next.next) {
-            if (f.next == null) break;
+        if(head == null) return false;
+        for(ListNode s=head, f=head.next; s!=null && f!=null && f.next!=null; s=s.next, f=f.next.next) {
             if(s==f) return true; // or s==f is faster than s.equals(f) as == compares the references
         }
         return false;
@@ -56,6 +57,16 @@ public class LinkedListCycle {
         return false;
     }
 
+    public static boolean hasCycleUsingSet(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (!set.add(head)) return true;
+            head = head.next;
+        }
+
+        return false;
+    }
+
     public static boolean hasCycleUsingHashMap(ListNode head) {
         Map<ListNode, Boolean> map = new HashMap<>();
         for(ListNode trav = head; trav != null; trav = trav.next) {
@@ -71,16 +82,6 @@ public class LinkedListCycle {
             if(lst.contains(trav)) return true;
             lst.add(trav);
         }
-        return false;
-    }
-
-    public static boolean hasCycleUsingSet(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        while (head != null) {
-            if (!set.add(head)) return true;
-            head = head.next;
-        }
-
         return false;
     }
 }
