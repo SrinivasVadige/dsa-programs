08d9d1e6d1a3e4e641db2ee035f5a5c4fe03b8f6
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-11-08 17:53:45 -0600

    Updated methods

--- Algorithms/LinkedListAlgos/RemoveNthNodeFromEndOfList.java
+++ Algorithms/LinkedListAlgos/RemoveNthNodeFromEndOfList.java
@@ -1,11 +1,19 @@
 package Algorithms.LinkedListAlgos;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Given the head of a linked list and an integer n, remove the nth node from the end of the list and return its head.
- * i.e remove node from right side and did not know the size of the LinkedList
+ * i.e., remove node from the right side and did not know the size of the LinkedList
+ * One pass means only for loop
+ * In-place means no extra space
  *
  * @author Srinivas Vadige, srinivas.vadige@gmail.com
  * @since 11 Nov 2024
+ * @link 19. Remove Nth Node From End of List <a href="https://leetcode.com/problems/remove-nth-node-from-end-of-list/">LeetCode Link</a>
+ * @topics Linked List, Two Pointers
  */
 public class RemoveNthNodeFromEndOfList {
     public static class ListNode {
@@ -17,27 +25,80 @@ public class RemoveNthNodeFromEndOfList {
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-
+        ListNode head;
         int n = 2;
 
-        System.out.println(removeNthFromEnd(head, n).val);
+        head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        System.out.println("Removed Node using two pointers - one pass, no memory => " + getNodeVal(removeNthFromEndUsingTwoPointers(head, n)));
 
         head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        System.out.println(removeNthFromEndMyApproach(head, n).val);
+        System.out.println("Removed Node using two pass, no memory => " + getNodeVal(removeNthFromEndUsingTwoPassNoMemory(head, n)));
+
+        head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        System.out.println("Removed Node using list => " + getNodeVal(removeNthFromEndUsingList(head, n)));
     }
 
-    public static ListNode removeNthFromEnd(ListNode head, int n) {
+    private static String getNodeVal (ListNode node) {return Optional.ofNullable(node).map(x -> x.val + "").orElse("null");}
+
+
+    /**
+     * @TimeComplexity O(n)
+     * @SpaceComplexity O(1)
+     * one pass, no memory
+
+        1 -> 2 -> 3 -> 4 -> 5
+        n = 2;        ⬆️
+
+        dummy -> 1 -> 2 -> 3 -> 4 -> 5 -> null
+           l     r
+
+        dummy -> 1 -> 2 -> 3 -> 4 -> 5 -> null
+           l               r
+
+        // NOTE: Now trav both "l" & "r", till "r" == null
+
+        dummy -> 1 -> 2 -> 3 -> 4 -> 5 -> null
+                           l               r
+
+        just skip the l's next node
+
+     */
+    public static ListNode removeNthFromEndUsingTwoPointers(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode l = dummy;
+        ListNode r = head;
+
+        for (; n > 0; n--) {
+            r = r.next;
+        }
+
+        /* NOTE: Now, the distance between l and r is "n+1" */
+
+        while (r != null) {
+            l = l.next;
+            r = r.next;
+        }
+
+        l.next = l.next.next;
+        return dummy.next;
+    }
+
+
+    /**
+     * @TimeComplexity O(n)
+     * @SpaceComplexity O(1)
+     */
+    public static ListNode removeNthFromEndUsingTwoPointers2(ListNode head, int n) {
         ListNode slow = head;
         ListNode fast = head;
 
-        for(int i=0; i<n; i++){ // skip uto n
+        for(int i=0; i<n; i++) { // skip uto n
             fast = fast.next;
         }
 
         if(fast == null) return head.next;
 
-        while(fast.next != null){ // i.e (length - n) node for slow pointer
+        while(fast.next != null) { // i.e (length - n) node for slow pointer
             slow = slow.next;
             fast = fast.next;
         }
@@ -47,26 +108,91 @@ public class RemoveNthNodeFromEndOfList {
         return head;
     }
 
-    public ListNode removeNthFromEnd2(ListNode head, int n) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode left = dummy;
-        ListNode right = head;
 
-        while (n > 0) {
-            right = right.next;
-            n--;
+
+
+
+
+    /**
+     * @TimeComplexity O(n)
+     * @SpaceComplexity O(1)
+     */
+    public static ListNode removeNthFromEndUsingTwoPassNoMemory(ListNode head, int n) {
+        // FIRST PASS / LOOP
+        int i = getLength(head) - n; // removeNode index or distance from removeNode
+        if (i==0) return head.next;
+
+        // SECOND PASS / LOOP
+        ListNode trav = head;
+        for (; trav != null && i > 1; trav = trav.next) {
+            i--;
         }
 
-        while (right != null) {
-            left = left.next;
-            right = right.next;
-        }
+        if (trav!=null && trav.next != null) trav.next = trav.next.next; // i==1 -> preNode - node before removeNode
 
-        left.next = left.next.next;
-        return dummy.next;
+        return head;
     }
 
-    public static ListNode removeNthFromEndMyApproach(ListNode head, int n) {
+
+
+    public static ListNode removeNthFromEndUsingTwoPassNoMemory2(ListNode head, int n) {
+        int i = getLength(head) - n; // removeNode index or distance from removeNode
+        if (i==0) return head.next;
+
+        for (ListNode trav = head; trav != null && i > 0; trav = trav.next, i--) {
+            if (i == 1) trav.next = trav.next.next; // preNode - node before removeNode
+        }
+
+        return head;
+    }
+
+
+
+    private static int getLength(ListNode node) {
+        int len = 0;
+        for (ListNode trav = node; trav != null; trav = trav.next) len++;
+        return len;
+    }
+
+
+
+
+
+
+
+
+    /**
+     * @TimeComplexity O(n)
+     * @SpaceComplexity O(n)
+     * One pass with O(n) space
+     */
+    public static ListNode removeNthFromEndUsingList(ListNode head, int n) {
+        List<ListNode> list = new ArrayList<>();
+        for (ListNode trav = head; trav != null; trav = trav.next) {
+            list.add(trav);
+        }
+
+        int i = list.size() - n - 1; // removeIndex - 1 === preRemoveNodeIndex
+        if (i == -1) return list.get(0).next;
+        list.get(i).next = list.get(i).next.next;
+        return head;
+    }
+
+
+
+
+
+
+
+
+
+
+    /**
+     * @TimeComplexity O(n)
+     * @SpaceComplexity O(1)
+     * Two pass
+     */
+    public static ListNode removeNthFromEndMyApproachOld(ListNode head, int n) {
         if (head.next == null && n==1) return null;
         ListNode sp=head, fp=head; // slow pointer and fast pointer
         int spIndex = 0;
@@ -102,7 +228,6 @@ public class RemoveNthNodeFromEndOfList {
     }
 
     private static void remove(ListNode node) {
-        if(node.next != null)
-            node.next = node.next.next;
+        if(node.next != null) node.next = node.next.next;
     }
 }
