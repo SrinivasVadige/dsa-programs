9255834df811c191b052c3550a58e689f26bc444
Srinivas Vadige <srinivas.vadige@gmail.com>
2025-11-18 18:08:45 -0600

    Updated methods

--- Algorithms/BinaryTrees/InvertBinaryTree.java
+++ Algorithms/BinaryTrees/InvertBinaryTree.java
@@ -9,11 +9,13 @@ import java.util.Set;
 import java.util.Stack;
 
 /**
-* @author Srinivas Vadige, srinivas.vadige@gmail.com
-* @since 23 Sept 2024
+ * @author Srinivas Vadige, srinivas.vadige@gmail.com
+ * @since 23 Sept 2024
+ * @link 226. Invert Binary Tree <a href="https://leetcode.com/problems/invert-binary-tree/">LeetCode Link</a>
+ * @topics Tree, BinaryTree, BFS, DFS, Recursion
 */
 public class InvertBinaryTree {
-    static class TreeNode {int val;TreeNode left, right;TreeNode() {}TreeNode(int val) { this.val = val; }TreeNode(int val, TreeNode left, TreeNode right) {this.val = val;this.left = left;this.right = right;}}
+    public static class TreeNode {int val;TreeNode left, right;TreeNode() {}TreeNode(int val) { this.val = val; }TreeNode(int val, TreeNode left, TreeNode right) {this.val = val;this.left = left;this.right = right;}}
     public static void main(String[] args) {
         /*
                    1
@@ -40,43 +42,30 @@ public class InvertBinaryTree {
         System.out.println("Original Tree: ");
         printTree(root).forEach(System.out::println);
 
-        root = invertTreeUsingSwapNodes(root);
-        System.out.println("invertTreeUsingSwapNodes: ");
-        printTree(root).forEach(System.out::println);
-
-        root = buildTree(nums);
         root = invertTreeUsingRecursion(root);
-        System.out.println("invertTreeUsingRecursion: ");
+        System.out.println("invertTree using recursion: ");
         printTree(root).forEach(System.out::println);
 
         root = buildTree(nums);
-        root = invertTreeUsingRecursion2(root);
-        System.out.println("invertTreeUsingRecursion2: ");
+        root = invertTreeUsingDfs(root);
+        System.out.println("invertTree using dfs: ");
         printTree(root).forEach(System.out::println);
 
         root = buildTree(nums);
-        root = invertTreeUsingStack(root);
-        System.out.println("invertTreeUsingStack: ");
+        root = invertTreeUsingBfs(root);
+        System.out.println("invertTree using bfs: ");
+        printTree(root).forEach(System.out::println);
+
+
+        root = buildTree(nums);
+        root = invertTreeUsingDfsStack(root);
+        System.out.println("invertTree using dfs stack: ");
         printTree(root).forEach(System.out::println);
     }
 
-
-    public static TreeNode invertTreeUsingSwapNodes(TreeNode root) {
-        swapNodes(root);
-        return root;
-    }
-
-    public static void swapNodes(TreeNode node){
-        if(node == null) return;
-        TreeNode temp=node.left;
-        node.left=node.right;
-        node.right=temp;
-        swapNodes(node.left);
-        swapNodes(node.right);
-    }
 
     public static TreeNode invertTreeUsingRecursion(TreeNode root) {
-        if (root == null) return root;
+        if (root == null) return null;
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
@@ -85,35 +74,76 @@ public class InvertBinaryTree {
         return root;
     }
 
+
+
     public static TreeNode invertTreeUsingRecursion2(TreeNode root) {
         if (root == null) return null;
-        TreeNode temp = root.left;
-        root.left = invertTreeUsingRecursion2(root.right);
-        root.right = invertTreeUsingRecursion2(temp);
+        TreeNode left = invertTreeUsingRecursion2(root.left);
+        TreeNode right = invertTreeUsingRecursion2(root.right);
+        root.left = right;
+        root.right = left;
         return root;
     }
 
 
-    public static TreeNode invertTreeUsingStack(TreeNode root) {
-        if (root == null) return root;
 
-        Set<TreeNode> visitedNodes = new HashSet<>();
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
 
-        while (!stack.empty()) {
-            TreeNode node = stack.pop();
-            visitedNodes.add(node);
 
+    public static TreeNode invertTreeUsingDfs(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+    private static void dfs(TreeNode node) { // swapNodes()
+        if(node == null) return;
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        dfs(node.left);
+        dfs(node.right);
+    }
+
+
+
+
+
+
+    public static TreeNode invertTreeUsingBfs(TreeNode root) {
+        if(root == null) return null;
+
+        Queue<TreeNode> q = new LinkedList<>(); // Note: q = new ArrayDeque<>(); q.add(null); throws NullPointerException
+        q.add(root);
+
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if(node == null) continue;
             TreeNode temp = node.left;
             node.left = node.right;
             node.right = temp;
-
-            if (node.right != null && !visitedNodes.contains(node.right))
-                stack.push(node.right);
-            if (node.left != null && !visitedNodes.contains(node.left))
-                stack.push(node.left);
+            q.add(node.left); // or if(node.left != null) q.add(node.left);
+            q.add(node.right);
         }
+
+        return root;
+    }
+
+
+
+    public static TreeNode invertTreeUsingDfsStack(TreeNode root) {
+        if(root == null) return null;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if(node == null) continue;
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            stack.push(node.left);
+            stack.push(node.right);
+        }
+
         return root;
     }
 
@@ -142,10 +172,8 @@ public class InvertBinaryTree {
         int i = 1;
         while (i < nums.length) {
             TreeNode curr = q.remove();
-            if (i < nums.length) {
-                curr.left = new TreeNode(nums[i++]);
-                q.add(curr.left);
-            }
+            curr.left = new TreeNode(nums[i++]);
+            q.add(curr.left);
             if (i < nums.length) {
                 curr.right = new TreeNode(nums[i++]);
                 q.add(curr.right);
@@ -183,9 +211,7 @@ public class InvertBinaryTree {
     }
 
     private static int getHeight(final TreeNode root) {
-        if(root == null)
-            return 0;
-
+        if(root == null) return 0;
         return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
     }
 
