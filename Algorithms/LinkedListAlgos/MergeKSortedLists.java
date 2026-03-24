44892f78fcd3325b8777a0aa877ab356ec66bc9a
Srinivas Vadige <srinivas.vadige@gmail.com>
2026-03-23 20:18:52 -0600

    LinkedList & Divide and Conquer: 23. Merge k Sorted Lists - Updated methods and documentation

--- Algorithms/LinkedListAlgos/MergeKSortedLists.java
+++ Algorithms/LinkedListAlgos/MergeKSortedLists.java
@@ -1,24 +1,22 @@
 package Algorithms.LinkedListAlgos;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Given the heads of k sorted linked lists list1, list2, ..., list k, return the sorted list
  * containing all the lists in sorted order.
  *
  *
- * @see Algorithms.LinkedListAlgos.MergeTwoSortedLists#mergeTwoListsUsingSort(Algorithms.LinkedListAlgos.MergeTwoSortedLists.ListNode, Algorithms.LinkedListAlgos.MergeTwoSortedLists.ListNode)
  * @author Srinivas Vadige, srinivas.vadige@gmail.com
  * @since 11 Nov 2024
+ * @link 23. Merge k Sorted Lists <a href="https://leetcode.com/problems/merge-k-sorted-lists/">LeetCode link</a>
+ * @topics Linked List, Divide and Conquer, Heap(Priority Queue), Merge Sort
+ * @companies Amazon(17), Meta(10), Microsoft(9), Bloomberg(5), Google(3), Cloudflare(3), Apple(2), Oracle(2), Nvidia(2), Rippling(6), TikTok(5), Deloitte(3), Flipkart(2), Snowflake(2), MongoDB(2), Yandex(6), Uber(5), Palantir Technologies(5), LinkedIn(4), Walmart Labs(4), WarnerMedia(4), Anduril(4), TCS(3), Goldman Sachs(3), SoFi(3)
+ * @see Algorithms.LinkedListAlgos.MergeTwoSortedLists#mergeTwoListsUsingSort(Algorithms.LinkedListAlgos.MergeTwoSortedLists.ListNode, Algorithms.LinkedListAlgos.MergeTwoSortedLists.ListNode)
  */
 public class MergeKSortedLists {
 
-    private static class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
@@ -31,19 +29,39 @@ public class MergeKSortedLists {
         ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
         ListNode list3 = new ListNode(2, new ListNode(6));
 
-        ListNode mergedList = mergeKListsUsingSort(new ListNode[]{list1, list2, list3});
-        for(ListNode trav = mergedList; trav != null; trav = trav.next) {
+        System.out.println("mergeKLists Using Sort: ");
+        for(ListNode trav = mergeKListsUsingListSortBruteForce(new ListNode[]{list1, list2, list3}); trav != null; trav = trav.next) {
             System.out.print(trav.val + " ");
         }
 
-        System.out.println();
-        mergedList = mergeKListsUsingBinaryTree(new ListNode[]{list1, list2, list3});
-        for(ListNode trav = mergedList; trav != null; trav = trav.next) {
+        System.out.println("\nmergeKLists Using Divide and Conquer Merge Sort Approach 1: ");
+        for(ListNode trav = mergeKListsUsingDivideAndConquerMergeSortApproach1(new ListNode[]{list1, list2, list3}); trav != null; trav = trav.next) {
+            System.out.print(trav.val + " ");
+        }
+
+        System.out.println("\nmergeKLists Using Divide and Conquer Merge Sort Approach 2: ");
+        for(ListNode trav = mergeKListsUsingDivideAndConquerMergeSortApproach2(new ListNode[]{list1, list2, list3}); trav != null; trav = trav.next) {
+            System.out.print(trav.val + " ");
+        }
+
+        System.out.println("\nmergeKLists Using Divide and Conquer Merge Sort Approach 3: ");
+        for(ListNode trav = mergeKListsUsingDivideAndConquerMergeSortApproach3(new ListNode[]{list1, list2, list3}); trav != null; trav = trav.next) {
+            System.out.print(trav.val + " ");
+        }
+
+        System.out.println("\nmergeKLists Using Priority Queue Optimized: ");
+        for(ListNode trav = mergeKListsUsingPriorityQueueOptimized(new ListNode[]{list1, list2, list3}); trav != null; trav = trav.next) {
             System.out.print(trav.val + " ");
         }
     }
 
-    public static ListNode mergeKListsUsingSort(ListNode[] lists) {
+
+
+    /**
+     * @TimeComplexity O(n log n)
+     * @SpaceComplexity O(n)
+     */
+    public static ListNode mergeKListsUsingListSortBruteForce(ListNode[] lists) {
         List<Integer> lst = new ArrayList<>();
         for(ListNode node: lists) {
             for(ListNode trav=node; trav!=null; trav=trav.next)
@@ -63,66 +81,186 @@ public class MergeKSortedLists {
         return dummy.next;
     }
 
-    /**
-     * Convert this to binary tree approach---> brute force
-     *
-     *        l1    l2     l3      l4     l5
-     *        |_____|       |       |      |
-     *           |__________|       |      |
-     *                 |____________|      |
-     *                        |____________|
-     *                               |
-     *                              res
-     *
-     * Convert this to binary tree best approach---> improved
-     *
-     *         l1    l2     l3      l4     l5
-     *          |_____|      |_______|      |
-     *             |_____________|          |
-     *                    |_________________|
-     *                              |
-     *                             res
-     */
-    public static ListNode mergeKListsUsingBinaryTree(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
 
-        while (lists.length > 1) {
-            List<ListNode> res = new ArrayList<>();
 
-            for (int i=0; i<lists.length; i+=2) { // do two and skip to third node
-                ListNode l1 = lists[i];
-                ListNode l2 = (i+1) < lists.length? lists[i+1]: null;
-                res.add(mergeTwoListNodes(l1, l2));
+
+
+        /**
+         * @TimeComplexity O(n * k), where n = total nodes and k = k lists - lists.length ===> ((((l1 + l2) + l3) + l4) + l5) ...
+         * Here we are re-merging the same nodes again and again
+         * (N/K) + (2N/K) + (3N/K) + ... + (KN/K)
+         * = N * (1 + 2 + 3 + ... + K) / K
+         * = N * (K(K+1)/2) / K
+         * O(N * K)
+         * @SpaceComplexity O(n)
+
+
+
+          Convert this to binary tree approach---> Divide and Conquer Brute force
+
+                 l1    l2     l3      l4     l5
+                 |_____|       |       |      |
+                    |__________|       |      |
+                          |____________|      |
+                                 |____________|
+                                        |
+                                       res
+            this {@link #mergeKListsUsingDivideAndConquerMergeSortApproach1} is slower than {@link #mergeKListsUsingDivideAndConquerMergeSortApproach2}
+            cause in this approach we're doing extra work of re-merging the same nodes again and again
+         */
+        public static ListNode mergeKListsUsingDivideAndConquerMergeSortApproach1(ListNode[] lists) { // MY APPROACH
+            if (lists.length == 0) return null;
+
+            ListNode dummy = new ListNode(), node = dummy;
+            ListNode left = lists[0];
+
+            for (int i=1; i<lists.length; i++) {
+                ListNode right = lists[i];
+                while (left != null && right != null) {
+                    if (left.val < right.val) {
+                        node.next = left;
+                        node = node.next;
+                        left = left.next;
+                    } else {
+                        node.next = right;
+                        node = node.next;
+                        right = right.next;
+                    }
+                }
+
+                if (left != null) node.next = left;
+                else node.next = right;
+
+                left = dummy.next;
+                dummy = new ListNode();
+                node = dummy;
+
             }
-            lists = res.toArray(ListNode[]::new);
+
+            return left;
+        }
+
+
+
+
+
+
+
+    /**
+     * @TimeComplexity O(n log k), where n = total nodes and k = k lists - lists.length
+     * Here instead of above approach we do → (l1 + l2), (l3 + l4), (l5 + l6) → then merge results → then merge again
+     * Each node is processed only log K times
+     * logK levels
+     * @SpaceComplexity O(n)
+
+        l1+l2   l3+l4   l5+l6
+          ↓       ↓       ↓
+           merge pairs evenly
+                 ↓
+            final merge
+
+
+     Convert this to binary tree best approach---> improved
+
+              l1    l2     l3      l4     l5
+               |_____|      |_______|      |
+              l1  |_____________| l3       |
+                      l1 |_________________| l5
+                                   |
+                                   l1
+
+     That's why this {@link #mergeKListsUsingDivideAndConquerMergeSortApproach2} is faster than {@link #mergeKListsUsingDivideAndConquerMergeSortApproach1}
+
+     */
+    public static ListNode mergeKListsUsingDivideAndConquerMergeSortApproach2(ListNode[] lists) {
+        int n = lists.length;
+        if (n == 0) return null;
+
+        while (n > 1) {
+            List<ListNode> mergedLists = new ArrayList<>(); // use int interval variable instead of mergedLists --> see {@link #mergeKListsUsingDivideAndConquerMergeSortApproach3}
+
+            for (int i=0; i<n; i+=2) { // do two and skip to third node
+                ListNode l1 = lists[i];
+                ListNode l2 = (i+1) < n? lists[i+1]: null; // for i == n-1, l2 = null
+                mergedLists.add(mergeTwoListNodes(l1, l2));
+            }
+            lists = mergedLists.toArray(ListNode[]::new);
+            n = lists.length;
         }
 
         return lists[0];
     }
 
     private static ListNode mergeTwoListNodes(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
+//        if (l1 == null) return l2;
+//        if (l2 == null) return l1;
 
-        ListNode head = new ListNode(), trav = head;
+        ListNode head = new ListNode(), tail = head;
         while(l1 != null && l2 !=null) {
             if(l1.val < l2.val) {
-                trav.next = l1;
+                tail.next = l1;
                 l1 = l1.next;
-                trav = trav.next;
             } else {
-                trav.next = l2;
+                tail.next = l2;
                 l2 = l2.next;
-                trav = trav.next;
+                /*
+                // or
+                tail.next = l2;
+                l2 = l1;
+                l1 = tail.next.next;
+                 */
             }
+            tail = tail.next;
         }
-        trav.next = (l1 == null)? l2: l1;
+        tail.next = (l1 == null)? l2: l1;
 
         return head.next;
     }
 
-    public static ListNode mergeKListsUsingBinaryTree2(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
+
+
+
+
+
+
+
+    /**
+     * @TimeComplexity O(n log k) where k = number of linked lists and O(log k) for every pop
+     * @SpaceComplexity O(1)
+
+
+        interval = 1 → merge pairs of size 1
+        interval = 2 → merge pairs of size 2
+        interval = 4 → merge pairs of size 4
+
+        i increments like
+        interval = 1 → i = 0, 2, 4....
+        interval = 2 → i = 0, 4, 8....
+        interval = 4 → i = 0, 8, 16....
+
+     */
+    public static ListNode mergeKListsUsingDivideAndConquerMergeSortApproach3(ListNode[] lists) {
+        int n = lists.length;
+        if (n == 0) return null;
+        int interval = 1;
+        while (interval < n) {
+            for (int i = 0; i < n-interval; i += interval*2) {
+                lists[i] = mergeTwoListNodes(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+        return lists[0];
+    }
+
+
+
+
+
+
+
+
+    public static ListNode mergeKListsUsingDivideAndConquerMergeSortApproach4(ListNode[] lists) {
+        if (lists.length == 0) return null;
 
         while (lists.length > 1) {
             ListNode[] mergedLists = new ListNode[lists.length / 2]; // we already know how many ListNodes we have in "Convert this to binary tree best approach---> improved"
@@ -132,9 +270,7 @@ public class MergeKSortedLists {
             lists = mergedLists;
         }
 
-        if (lists.length == 1) return lists[0];
-
-        return null;
+        return lists[0];
     }
 
     private static ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
@@ -149,6 +285,17 @@ public class MergeKSortedLists {
         }
     }
 
+
+
+
+
+
+
+
+    /**
+     * @TimeComplexity O(n log n)
+     * @SpaceComplexity O(n)
+     */
     public ListNode mergeKListsUsingPriorityQueue(ListNode[] lists) {
         PriorityQueue<Integer> max = new PriorityQueue<>();
         for(ListNode node : lists){
@@ -166,6 +313,103 @@ public class MergeKSortedLists {
         }
         return dummy.next;
     }
+
+
+
+
+
+    /**
+     * @TimeComplexity O(n log k) where k = number of linked lists and O(log k) for every pop
+     * @SpaceComplexity O(n)
+     */
+    public static ListNode mergeKListsUsingPriorityQueueOptimized(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+            new Comparator<>() {
+                @Override
+                public int compare(ListNode o1, ListNode o2) {
+                    if (o1.val > o2.val) {
+                        return 1;
+                    } else if (o1.val == o2.val) {
+                        return 0;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+            /*
+            // or
+                (o1, o2) -> {
+                    if (o1.val > o2.val) {
+                        return 1;
+                    } else if (o1.val == o2.val) {
+                        return 0;
+                    } else {
+                        return -1;
+                    }
+                }
+
+
+             */
+        );
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(node);
+            }
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (!pq.isEmpty()) {
+            tail.next = pq.poll();
+            tail = tail.next;
+            if (tail.next != null) {
+                pq.add(tail.next);
+            }
+        }
+        return head.next;
+    }
+
+
+
+
+
+
+    /**
+     * @TimeComplexity O(n+r) -> n = total nodes, r = range of values == max-min+1 == Bucket size
+     * @SpaceComplexity O(r)
+     */
+    public static ListNode mergeKListsUsingBucketSort(ListNode[] lists) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (ListNode list : lists) {
+            while (list != null) {
+                min = Math.min(min, list.val);
+                max = Math.max(max, list.val);
+                list = list.next;
+            }
+        }
+        int[] bucket = new int[max - min + 1];
+        for (ListNode list : lists) {
+            while (list != null) {
+                bucket[list.val - min]++;
+                list = list.next;
+            }
+        }
+        ListNode dummy = new ListNode(-1), trav = dummy;
+        for (int i = 0; i < bucket.length; i++) {
+            while (bucket[i]-- > 0) {
+                trav.next = new ListNode(i + min);
+                trav = trav.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    
+
+
+
+
+
 
     public static ListNode mergeKListsUsingPriorityQueue2(ListNode[] lists) {
         Queue<ListNode> queue = new PriorityQueue<>((a, b) -> a.val - b.val);
@@ -185,6 +429,12 @@ public class MergeKSortedLists {
         }
         return dummy.next;
     }
+
+
+
+
+
+
 
 
 
@@ -230,6 +480,12 @@ public class MergeKSortedLists {
         }
         return result.next;
     }
+
+
+
+
+
+
 
     public ListNode mergeKListsUsingArrayDeque(ListNode[] lists) {
         Queue<ListNode> queue = new ArrayDeque<>();
