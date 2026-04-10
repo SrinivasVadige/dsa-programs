7c262768ad618c4ada817a7556bc95ef58d6ae87
Srinivas Vadige <srinivas.vadige@gmail.com>
2026-04-09 20:05:21 -0600

    added new method

--- Algorithms/BinarySearch/SearchInRotatedSortedArray.java
+++ Algorithms/BinarySearch/SearchInRotatedSortedArray.java
@@ -16,7 +16,7 @@ public class SearchInRotatedSortedArray {
         int target = 1;
         System.out.println("binarySearch using find pivot index and binary search => " + searchUsingFindPivotIndexAndBinarySearch1(nums, target));
         System.out.println("binarySearch using find pivot index and binary search with shift => " + searchUsingFindPivotIndexAndBinarySearchWithShift(nums, target));
-        System.out.println("binarySearch using one binary search => " + searchUsingOneBinarySearch(nums, target));
+        System.out.println("binarySearch using one binary search => " + searchUsingOneBinarySearch1(nums, target));
     }
 
 
@@ -136,17 +136,39 @@ public class SearchInRotatedSortedArray {
      * @TimeComplexity O(log n)
      * @SpaceComplexity O(1)
      */
-    public static int searchUsingOneBinarySearch(int[] nums, int target) {
+    public static int searchUsingOneBinarySearch1(int[] nums, int target) {
         int l = 0, r = nums.length - 1;
         while (l <= r) {
             int mid = (l + r) / 2;
             if (nums[mid] == target) return mid;
             if (nums[l] <= nums[mid]) { // isLeftPortionSorted?
-                if (target >= nums[l] && target < nums[mid]) r = mid - 1; // target is in left portion
+                if (nums[l] <= target && target < nums[mid]) r = mid - 1; // target is in left portion
                 else l = mid + 1;
-            } else {
-                if (target > nums[mid] && target <= nums[r]) l = mid + 1; // target is in right portion && isRightPortionSorted?
+            } else { // isRightPortionSorted?
+                if (nums[mid] < target && target <= nums[r]) l = mid + 1; // target is in right portion && isRightPortionSorted?
                 else r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+
+    /**
+     * @TimeComplexity O(log n)
+     * @SpaceComplexity O(1)
+     */
+    public static int searchUsingOneBinarySearch2(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[l] <= nums[mid]) { // isLeftPortionSorted?
+                if (target < nums[l] || nums[mid] < target) l = mid + 1; // target is in left portion
+                else r = mid - 1;
+            } else { // isRightPortionSorted?
+                if (target < nums[mid] || nums[r] < target) r = mid - 1; // target is in right portion && isRightPortionSorted?
+                else l = mid + 1;
             }
         }
         return -1;
